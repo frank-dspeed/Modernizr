d831e42bae5cd8d2dc00b8945bc4ad08407ed661
Frank Lemanschik <frank@dspeed.eu>
2019-10-06 16:55:20 +0200

    Fix: Meta

--- feature-detects/script/type_module.js
+++ feature-detects/script/type_module.js
@@ -5,7 +5,7 @@
   "caniuse": "script-type-module",
   "tags": ["script"],
   "builderAliases": ["script_type_module"],
-  "authors": ["Frank Lemanschik"]
+  "authors": ["Frank Lemanschik"],
   "notes": [{
      "name": "The ES6 Dynamic Import spec",
      "href": "https://tc39.github.io/ecma262/#sec-modules"
